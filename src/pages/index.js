9163ca89e2e3acccfbd22e18a3f1b6853a589d4a
Joe Liang <56658669+1000mileworld@users.noreply.github.com>
2020-04-11 16:43:24 -0400

    move mapEffect into class component

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,12 +21,12 @@ const mapSettings = {
   center: CENTER,
   defaultBaseMap: 'OpenStreetMap',
   zoom: DEFAULT_ZOOM,
-  mapEffect
+  mapEffect: IndexPage.mapEffect3 //reference to function, not calling it
 };
 
 let response;
 
-let test;
+let option = 0;
 function log(test){
   console.log(test);
 }
@@ -42,10 +42,10 @@ const sources = [
    * @description Fires a callback once the page renders
    * @example Here this is an example of being used to zoom in and set a popup on load
    */
-async function mapEffect ({leafletElement: map}) {
+async function mapEffect2 ({leafletElement: map}, option=1) {
     
   try {
-    response = await axios.get(sources[1]);
+    response = await axios.get(sources[option]);
   } catch(e) {
     console.log(`Failed to fetch countries: ${e.message}`, e);
     return;
@@ -137,9 +137,97 @@ class IndexPage extends React.Component  {
     this.handleClick = this.handleChange.bind(this);
   }
   
+  mapEffect3 = async({leafletElement: map}, option=1) => {
+    
+    try {
+      response = await axios.get(sources[option]);
+    } catch(e) {
+      console.log(`Failed to fetch countries: ${e.message}`, e);
+      return;
+    }
+  
+    const { data = [] } = response;
+    //console.log(data);
+  
+    const hasData = Array.isArray(data) && data.length > 0;
+  
+    if ( !hasData ) return;
+  
+    const geoJson = {
+      type: 'FeatureCollection',
+      features: data.map((country = {}) => {
+        const { countryInfo = {} } = country;
+        const { lat, long: lng } = countryInfo;
+        return {
+          type: 'Feature',
+          properties: {
+            ...country,
+          },
+          geometry: {
+            type: 'Point',
+            coordinates: [ lng, lat ]
+          }
+        }
+      })
+    }
+  
+    //console.log(geoJson);
+  
+    //create hover over icon on map using Leaflet
+    const geoJsonLayers = new L.GeoJSON(geoJson, {
+      pointToLayer: (feature = {}, latlng) => {
+        const { properties = {} } = feature;
+        let updatedFormatted;
+        let casesString;
+    
+        const {
+          country,
+          updated,
+          cases,
+          deaths,
+          recovered
+        } = properties
+    
+        casesString = `${cases}`;
+    
+        if ( cases > 1000 ) {
+          casesString = `${casesString.slice(0, -3)}k+`
+        }
+    
+        if ( updated ) {
+          updatedFormatted = new Date(updated).toLocaleString();
+        }
+    
+        const html = `
+          <span class="icon-marker">
+            <span class="icon-marker-tooltip">
+              <h2>${country}</h2>
+              <ul>
+                <li><strong>Confirmed:</strong> ${cases}</li>
+                <li><strong>Deaths:</strong> ${deaths}</li>
+                <li><strong>Recovered:</strong> ${recovered}</li>
+                <li><strong>Last Update:</strong> ${updatedFormatted}</li>
+              </ul>
+            </span>
+            ${ casesString }
+          </span>
+        `;
+    
+        return L.marker( latlng, {
+          icon: L.divIcon({
+            className: 'icon',
+            html
+          }),
+          riseOnHover: true
+        });
+      }
+    });
+    geoJsonLayers.addTo(map);
+  }
+
   handleChange(e){
-    test=e.target.value;
-    log(test);
+    option=e.target.value;
+    log(option);
   }
   render() {
     return (
