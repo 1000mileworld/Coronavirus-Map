d1da7dc1e4607ad7485e74f99303f227d020f4e6
Joe Liang <56658669+1000mileworld@users.noreply.github.com>
2020-04-11 19:27:18 -0400

    added mapbox style

--- src/pages/index.js
+++ src/pages/index.js
@@ -131,6 +131,19 @@ class IndexPage extends React.Component  {
       }
     });
     geoJsonLayers.addTo(map);
+
+    const token = '';
+
+    //map.setView([51.505, -0.09], 13);
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox/satellite-streets-v11',
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken: token
+    }).addTo(map);
+    
   }
 
   handleChange = (e) => {
