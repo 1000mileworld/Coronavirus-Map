6d14050585837913e7310ffcbbc8ec52e1b3edb8
Joe Liang <56658669+1000mileworld@users.noreply.github.com>
2020-04-11 17:46:31 -0400

    all workinggit add .

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import Helmet from 'react-helmet';
 import L from 'leaflet';
@@ -17,19 +16,7 @@ const LOCATION = {
 const CENTER = [LOCATION.lat, LOCATION.lng];
 const DEFAULT_ZOOM = 2;
 
-const mapSettings = {
-  center: CENTER,
-  defaultBaseMap: 'OpenStreetMap',
-  zoom: DEFAULT_ZOOM,
-  mapEffect: IndexPage.mapEffect3 //reference to function, not calling it
-};
-
 let response;
-
-let option = 0;
-function log(test){
-  console.log(test);
-}
 
 const sources = [
   'https://corona.lmao.ninja/countries',
@@ -37,110 +24,31 @@ const sources = [
   'https://gist.githubusercontent.com/1000mileworld/0f12e488ac84b9bdc7e29638be62454f/raw/a5fa43da7adb5b7f351f3a81354e6eebcc05a570/Coronavirus%2520stat%25202020-04-09'
 ];
 
-/**
-   * mapEffect
-   * @description Fires a callback once the page renders
-   * @example Here this is an example of being used to zoom in and set a popup on load
-   */
-async function mapEffect2 ({leafletElement: map}, option=1) {
-    
-  try {
-    response = await axios.get(sources[option]);
-  } catch(e) {
-    console.log(`Failed to fetch countries: ${e.message}`, e);
-    return;
-  }
-
-  const { data = [] } = response;
-  //console.log(data);
-
-  const hasData = Array.isArray(data) && data.length > 0;
-
-  if ( !hasData ) return;
-
-  const geoJson = {
-    type: 'FeatureCollection',
-    features: data.map((country = {}) => {
-      const { countryInfo = {} } = country;
-      const { lat, long: lng } = countryInfo;
-      return {
-        type: 'Feature',
-        properties: {
-          ...country,
-        },
-        geometry: {
-          type: 'Point',
-          coordinates: [ lng, lat ]
-        }
-      }
-    })
-  }
-
-  //console.log(geoJson);
-
-  //create hover over icon on map using Leaflet
-  const geoJsonLayers = new L.GeoJSON(geoJson, {
-    pointToLayer: (feature = {}, latlng) => {
-      const { properties = {} } = feature;
-      let updatedFormatted;
-      let casesString;
-  
-      const {
-        country,
-        updated,
-        cases,
-        deaths,
-        recovered
-      } = properties
-  
-      casesString = `${cases}`;
-  
-      if ( cases > 1000 ) {
-        casesString = `${casesString.slice(0, -3)}k+`
-      }
-  
-      if ( updated ) {
-        updatedFormatted = new Date(updated).toLocaleString();
-      }
-  
-      const html = `
-        <span class="icon-marker">
-          <span class="icon-marker-tooltip">
-            <h2>${country}</h2>
-            <ul>
-              <li><strong>Confirmed:</strong> ${cases}</li>
-              <li><strong>Deaths:</strong> ${deaths}</li>
-              <li><strong>Recovered:</strong> ${recovered}</li>
-              <li><strong>Last Update:</strong> ${updatedFormatted}</li>
-            </ul>
-          </span>
-          ${ casesString }
-        </span>
-      `;
-  
-      return L.marker( latlng, {
-        icon: L.divIcon({
-          className: 'icon',
-          html
-        }),
-        riseOnHover: true
-      });
-    }
-  });
-  geoJsonLayers.addTo(map);
-}
-
 class IndexPage extends React.Component  {
 
   constructor(props) {
     super(props);
+    this.state={
+      mapSettings: {
+          center: CENTER,
+          defaultBaseMap: 'OpenStreetMap',
+          zoom: DEFAULT_ZOOM,
+          mapEffect: this.mapEffect //reference to function, not calling it
+        },
+      option: 0
+    }
     this.handleClick = this.handleChange.bind(this);
   }
   
-  mapEffect3 = async({leafletElement: map}, option=1) => {
+  /**
+   * mapEffect
+   * @description Fires a callback once the page renders
+   * @example Here this is an example of being used to zoom in and set a popup on load
+   */
+  mapEffect = async({leafletElement: map}) => {
     
     try {
-      response = await axios.get(sources[option]);
+      response = await axios.get(sources[this.state.option]);
     } catch(e) {
       console.log(`Failed to fetch countries: ${e.message}`, e);
       return;
@@ -225,10 +133,12 @@ class IndexPage extends React.Component  {
     geoJsonLayers.addTo(map);
   }
 
-  handleChange(e){
-    option=e.target.value;
-    log(option);
+  handleChange = (e) => {
+    this.setState({
+      option: parseInt(e.target.value)
+    });  
   }
+
   render() {
     return (
       <Layout pageName="home">
@@ -236,7 +146,8 @@ class IndexPage extends React.Component  {
           <title>Coronavirus Tracker</title>
         </Helmet>
 
-        <Map {...mapSettings}/>
+        {/* Need key attribute to force component to rerender on state update (React uses the key prop to understand the component-to-DOM Element relation) */}
+        <Map {...this.state.mapSettings} key={this.state.option}/> 
 
         <Container type="content" className="text-center home-start">
           <h2>Wordwide Coronavirus/COVID-19 Case Tracker</h2>
