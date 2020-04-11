d5f573025b25d78a3dde358e6f884542bd1e6954
Joe Liang <56658669+1000mileworld@users.noreply.github.com>
2020-04-11 15:49:15 -0400

    refactored mapEffect function to outside React class

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,131 +17,151 @@ const LOCATION = {
 const CENTER = [LOCATION.lat, LOCATION.lng];
 const DEFAULT_ZOOM = 2;
 
-const IndexPage = () => {
+const mapSettings = {
+  center: CENTER,
+  defaultBaseMap: 'OpenStreetMap',
+  zoom: DEFAULT_ZOOM,
+  mapEffect
+};
 
-  /**
+let response;
+
+let test;
+function log(test){
+  console.log(test);
+}
+
+const sources = [
+  'https://corona.lmao.ninja/countries',
+  'https://gist.githubusercontent.com/1000mileworld/157eb3485ad861f83dc50716531892c4/raw/9b5b58a6558edbf1a33f355a77c10eaf21d3a404/coronavirus%2520stat%25202020-04-08',
+  'https://gist.githubusercontent.com/1000mileworld/0f12e488ac84b9bdc7e29638be62454f/raw/a5fa43da7adb5b7f351f3a81354e6eebcc05a570/Coronavirus%2520stat%25202020-04-09'
+];
+
+/**
    * mapEffect
    * @description Fires a callback once the page renders
    * @example Here this is an example of being used to zoom in and set a popup on load
    */
+async function mapEffect ({leafletElement: map}) {
+    
+  try {
+    response = await axios.get(sources[1]);
+  } catch(e) {
+    console.log(`Failed to fetch countries: ${e.message}`, e);
+    return;
+  }
 
-  async function mapEffect({leafletElement: map}) {
-    let response;
+  const { data = [] } = response;
+  //console.log(data);
 
-    //https://corona.lmao.ninja/countries
-    //04-08: https://gist.githubusercontent.com/1000mileworld/157eb3485ad861f83dc50716531892c4/raw/9b5b58a6558edbf1a33f355a77c10eaf21d3a404/coronavirus%2520stat%25202020-04-08
-    try {
-      response = await axios.get('https://gist.githubusercontent.com/1000mileworld/0f12e488ac84b9bdc7e29638be62454f/raw/a5fa43da7adb5b7f351f3a81354e6eebcc05a570/Coronavirus%2520stat%25202020-04-09');
-    } catch(e) {
-      console.log(`Failed to fetch countries: ${e.message}`, e);
-      return;
-    }
+  const hasData = Array.isArray(data) && data.length > 0;
 
-    const { data = [] } = response;
-    //console.log(data);
+  if ( !hasData ) return;
 
-    const hasData = Array.isArray(data) && data.length > 0;
-
-    if ( !hasData ) return;
-
-    const geoJson = {
-      type: 'FeatureCollection',
-      features: data.map((country = {}) => {
-        const { countryInfo = {} } = country;
-        const { lat, long: lng } = countryInfo;
-        return {
-          type: 'Feature',
-          properties: {
-            ...country,
-          },
-          geometry: {
-            type: 'Point',
-            coordinates: [ lng, lat ]
-          }
+  const geoJson = {
+    type: 'FeatureCollection',
+    features: data.map((country = {}) => {
+      const { countryInfo = {} } = country;
+      const { lat, long: lng } = countryInfo;
+      return {
+        type: 'Feature',
+        properties: {
+          ...country,
+        },
+        geometry: {
+          type: 'Point',
+          coordinates: [ lng, lat ]
         }
-      })
-    }
-
-    //console.log(geoJson);
-
-    //create hover over icon on map using Leaflet
-    const geoJsonLayers = new L.GeoJSON(geoJson, {
-      pointToLayer: (feature = {}, latlng) => {
-        const { properties = {} } = feature;
-        let updatedFormatted;
-        let casesString;
-    
-        const {
-          country,
-          updated,
-          cases,
-          deaths,
-          recovered
-        } = properties
-    
-        casesString = `${cases}`;
-    
-        if ( cases > 1000 ) {
-          casesString = `${casesString.slice(0, -3)}k+`
-        }
-    
-        if ( updated ) {
-          updatedFormatted = new Date(updated).toLocaleString();
-        }
-    
-        const html = `
-          <span class="icon-marker">
-            <span class="icon-marker-tooltip">
-              <h2>${country}</h2>
-              <ul>
-                <li><strong>Confirmed:</strong> ${cases}</li>
-                <li><strong>Deaths:</strong> ${deaths}</li>
-                <li><strong>Recovered:</strong> ${recovered}</li>
-                <li><strong>Last Update:</strong> ${updatedFormatted}</li>
-              </ul>
-            </span>
-            ${ casesString }
-          </span>
-        `;
-    
-        return L.marker( latlng, {
-          icon: L.divIcon({
-            className: 'icon',
-            html
-          }),
-          riseOnHover: true
-        });
       }
-    });
-    geoJsonLayers.addTo(map);
+    })
+  }
+
+  //console.log(geoJson);
+
+  //create hover over icon on map using Leaflet
+  const geoJsonLayers = new L.GeoJSON(geoJson, {
+    pointToLayer: (feature = {}, latlng) => {
+      const { properties = {} } = feature;
+      let updatedFormatted;
+      let casesString;
+  
+      const {
+        country,
+        updated,
+        cases,
+        deaths,
+        recovered
+      } = properties
+  
+      casesString = `${cases}`;
+  
+      if ( cases > 1000 ) {
+        casesString = `${casesString.slice(0, -3)}k+`
+      }
+  
+      if ( updated ) {
+        updatedFormatted = new Date(updated).toLocaleString();
+      }
+  
+      const html = `
+        <span class="icon-marker">
+          <span class="icon-marker-tooltip">
+            <h2>${country}</h2>
+            <ul>
+              <li><strong>Confirmed:</strong> ${cases}</li>
+              <li><strong>Deaths:</strong> ${deaths}</li>
+              <li><strong>Recovered:</strong> ${recovered}</li>
+              <li><strong>Last Update:</strong> ${updatedFormatted}</li>
+            </ul>
+          </span>
+          ${ casesString }
+        </span>
+      `;
+  
+      return L.marker( latlng, {
+        icon: L.divIcon({
+          className: 'icon',
+          html
+        }),
+        riseOnHover: true
+      });
+    }
+  });
+  geoJsonLayers.addTo(map);
 }
 
-  const mapSettings = {
-    center: CENTER,
-    defaultBaseMap: 'OpenStreetMap',
-    zoom: DEFAULT_ZOOM,
-    mapEffect
-  };
+class IndexPage extends React.Component  {
 
-  return (
-    <Layout pageName="home">
-      <Helmet>
-        <title>Coronavirus Tracker</title>
-      </Helmet>
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleChange.bind(this);
+  }
+  
+  handleChange(e){
+    test=e.target.value;
+    log(test);
+  }
+  render() {
+    return (
+      <Layout pageName="home">
+        <Helmet>
+          <title>Coronavirus Tracker</title>
+        </Helmet>
 
-      <Map {...mapSettings}/>
+        <Map {...mapSettings}/>
 
-      <Container type="content" className="text-center home-start">
-        <h2>Wordwide Coronavirus/COVID-19 Case Tracker</h2>
-        <p>Select from past saved data or get most recent:</p>
-        <select className="select-css">
-          <option value="0">Most recent</option>
-          <option value="1">April 8, 2020</option>
-          <option value="2">April 9, 2020</option>
-        </select>
-      </Container>
-    </Layout>
-  );
+        <Container type="content" className="text-center home-start">
+          <h2>Wordwide Coronavirus/COVID-19 Case Tracker</h2>
+          <p>Select from past saved data or get most recent:</p>
+          <select id="dropdown" className="select-css" onChange={this.handleChange}>
+            <option value="0">Most recent</option>
+            <option value="1">April 8, 2020</option>
+            <option value="2">April 9, 2020</option>
+          </select>
+        </Container>
+      </Layout>
+    );
+  }
 };
 
 export default IndexPage;
