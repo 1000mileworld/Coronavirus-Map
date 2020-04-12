fe63f43f99ebecfa1277df9ebd4830c64222a632
Joe Liang <56658669+1000mileworld@users.noreply.github.com>
2020-04-12 15:20:44 -0400

    add blog link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <footer>
       <Container>
-        <p>&copy; { new Date().getFullYear() } by Joe Liang</p>
+        <p>&copy; { new Date().getFullYear() } by Joe Liang - <a href="https://1000mileworld.com" target="_blank">1000 Mile World</a></p>
       </Container>
     </footer>
   );
